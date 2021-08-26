e7eb138d26cb2cfd9c428765c5df0a2b91a04774
sapkotarujal <sapkota.rujal@webexpertsnepal.com>
2021-08-26 09:39:35 +0545

    some modification on app table

--- src/components/ApplicationTable/Row.js
+++ src/components/ApplicationTable/Row.js
@@ -1,49 +1,28 @@
 import React from "react";
 import { TableCell, TableRow } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { ReactComponent as DeleteIcon } from "assets/icons/deleteIcon.svg";
 import IOSSwitch from "components/IOSSwitch";
-import ColourConstants from "helpers/colourConstants";
 
-const useStyles = makeStyles((theme) => ({
-	deleteIcon: {
-		transform: "scale(0.7)",
-		color: ColourConstants.deleteButton,
-		"&:hover": {
-			cursor: "pointer",
-		},
-		verticalAlign: "middle",
-	},
-	appName: {
-		color: "#307AD6",
-		textDecoration: "underline",
-		wordBreak: "break-word",
-	},
-}));
-
-const Row = ({ row, onDeleteApp, onChangeApp }) => {
-	const classes = useStyles();
-	return (
-		<TableRow>
-			<TableCell>
-				<span className={classes.appName}>{row.name}</span>
-			</TableCell>
-			<TableCell>{row.totalSites}</TableCell>
-			<TableCell
-				style={{
-					paddingLeft: 90,
-					width: "245px",
-				}}
-			>
-				<IOSSwitch onChange={onChangeApp} currentStatus={row.isActive} /> &nbsp;
-				{!row.isActive ? (
-					<DeleteIcon className={classes.deleteIcon} onClick={onDeleteApp} />
-				) : (
-					<div style={{ width: "32px" }}></div>
-				)}
-			</TableCell>
-		</TableRow>
-	);
-};
+const Row = ({ row, classes, onDeleteApp, onChangeApp }) => (
+	<TableRow>
+		<TableCell>
+			<span className={classes.appName}>{row.name}</span>
+		</TableCell>
+		<TableCell>{row.totalSites}</TableCell>
+		<TableCell
+			style={{
+				paddingLeft: 90,
+				width: "245px",
+			}}
+		>
+			<IOSSwitch onChange={onChangeApp} currentStatus={row.isActive} /> &nbsp;
+			{!row.isActive ? (
+				<DeleteIcon className={classes.deleteIcon} onClick={onDeleteApp} />
+			) : (
+				<div style={{ width: "32px" }}></div>
+			)}
+		</TableCell>
+	</TableRow>
+);
 
 export default Row;
