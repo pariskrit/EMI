2b041841bb60373b2535992a8be85fbc9bc0cbd6
sapkotarujal <sapkota.rujal@webexpertsnepal.com>
2021-08-28 08:38:30 +0545

    resolved the issue of site wrapper and sorting

---
+++ src/routes/Clients/Sites/SiteDetail/Details.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import AccordionBox from "components/AccordionBox";
+import SiteDetails from "components/SiteDetails";
+import KeyContacts from "./KeyContacts";
+import Applications from "./Applications";
+const Details = () => {
+	return (
+		<Grid container>
+			<Grid item xs={12}>
+				<Grid container spacing={2}>
+					<Grid item xs={12}>
+						<AccordionBox title="Site Details">
+							<SiteDetails
+								onChange={(e) => console.log(e.target.value)}
+								value="Boddington"
+							/>
+						</AccordionBox>
+					</Grid>
+					<Grid item xs={12}>
+						<KeyContacts />
+					</Grid>
+					<Grid item xs={12}>
+						<Applications />
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default Details;
