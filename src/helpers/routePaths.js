190601d161d64c83f471f77f7ca2600a43130708
Pariskrit19 <moktanpariskrit@gmail.com>
2021-09-22 12:22:16 +0545

    merged finally

--- src/helpers/routePaths.js
+++ src/helpers/routePaths.js
@@ -17,10 +17,8 @@ export const siteApplicationPathCustomCaptions =
 	siteApplicationPath + "/customcaptions";
 export const siteApplicationPathStopsReasons =
 	siteApplicationPath + "/stopsreasons";
-<<<<<<< HEAD
 export const siteApplicationOperationModesPath =
 	siteApplicationPath + "/operatingmodes";
-=======
 export const sitApplicationPathModelStatuses =
 	siteApplicationPath + "/modelstatuses";
 export const siteApplicationPathModelTypes =
@@ -31,7 +29,6 @@ export const siteApplicationPathMissingItems =
 	siteApplicationPath + "/missingitems";
 export const siteApplicationPathStatusChanges =
 	siteApplicationPath + "/statuschanges";
->>>>>>> pariskrit
 
 //Applications
 export const applicationListPath = "/applications";
