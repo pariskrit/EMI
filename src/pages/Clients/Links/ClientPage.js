c0f4782a506e9e8dc905dfe9cf9129a430c0f03a
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2021-10-28 17:07:37 +0545

    working on protected route

--- src/pages/Clients/Links/ClientPage.js
+++ src/pages/Clients/Links/ClientPage.js
@@ -1,19 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import { clientDetailPath, clientsPath } from "helpers/routePaths";
 import ClientList from "pages/Clients/ClientList/ClientList";
 import Client from "..";
 import ClientDetails from "../ClientDetailScreen/ClientDetails";
-import ProtectedRoute from "components/HOC/ProtectedRoute";
 
 export default function ClientPage() {
 	return (
 		<Client>
-			<ProtectedRoute path={clientsPath} exact>
+			<Route path={clientsPath} exact>
 				<ClientList />
-			</ProtectedRoute>
-			<ProtectedRoute path={clientDetailPath} exact>
+			</Route>
+			<Route path={clientDetailPath} exact>
 				<ClientDetails />
-			</ProtectedRoute>
+			</Route>
 		</Client>
 	);
 }
