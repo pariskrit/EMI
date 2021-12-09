5fd53455c861343fd5913fddd25ab82518f762e0
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2021-12-09 16:14:34 +0545

    added seperate style for the

--- src/components/Modules/ModelTaskTable/ModelTaskDetail.js
+++ src/components/Modules/ModelTaskTable/ModelTaskDetail.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useStyles } from "./styles";
 
 const ModelTaskDetail = () => {
+	const classes = useStyles();
 	return (
-		<div style={{ background: "#FFFFFF" }}>
+		<div className={classes.main}>
 			<h1>Hello World</h1>
 		</div>
 	);
