1b935feba0a9124bd00180ee47db75c960b4c5b1
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2021-12-13 11:23:04 +0545

    added route protected roles based on client site

--- src/pages/Clients/Sites/Links/SitePage.js
+++ src/pages/Clients/Sites/Links/SitePage.js
@@ -1,33 +1,26 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import {
-	siteAssetPath,
-	siteDepartmentPath,
-	siteDetailPath,
-	siteLocationPath,
-	sitePath,
-} from "helpers/routePaths";
-import SiteAsset from "pages/Clients/Sites/SiteAsset";
-import SiteDepartmentsScreen from "pages/Clients/Sites/SiteDepartment/SiteDepartmentsScreen";
-import SiteDetail from "pages/Clients/Sites/SiteDetail/SiteDetailsScreen";
-import SiteLocationsScreen from "pages/Clients/Sites/SiteLocations/SiteLocationsScreen";
+// import { Route } from "react-router-dom";
+// import {
+// 	siteAssetPath,
+// 	siteDepartmentPath,
+// 	siteDetailPath,
+// 	siteLocationPath,
+// 	sitePath,
+// } from "helpers/routePaths";
+// import SiteAsset from "pages/Clients/Sites/SiteAsset";
+// import SiteDepartmentsScreen from "pages/Clients/Sites/SiteDepartment/SiteDepartmentsScreen";
+// import SiteDetail from "pages/Clients/Sites/SiteDetail/SiteDetailsScreen";
+// import SiteLocationsScreen from "pages/Clients/Sites/SiteLocations/SiteLocationsScreen";
 import Site from "pages/Clients/Sites";
+import { routeList } from "./routeList";
+import AccessRoute from "components/HOC/AccessRoute";
 
 export default function SitePage() {
 	return (
 		<Site>
-			<Route path={sitePath + siteDetailPath} exact>
-				<SiteDetail />
-			</Route>
-			<Route path={sitePath + siteAssetPath} exact>
-				<SiteAsset />
-			</Route>
-			<Route path={sitePath + siteDepartmentPath} exact>
-				<SiteDepartmentsScreen />
-			</Route>
-			<Route path={sitePath + siteLocationPath} exact>
-				<SiteLocationsScreen />
-			</Route>
+			{routeList.map(({ id, ...route }) => (
+				<AccessRoute key={id} {...route} exact />
+			))}
 		</Site>
 	);
 }
