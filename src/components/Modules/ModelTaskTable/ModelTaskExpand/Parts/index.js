27b4315121768ebe52a5b3a759683efec0832ad8
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2021-12-14 14:54:15 +0545

    removed dnd table file and model task table with story

--- src/components/Modules/ModelTaskTable/ModelTaskExpand/Parts/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Parts = () => {
-	return <h1>Parts</h1>;
-};
-
-export default Parts;
--- src/components/Modules/ModelTaskTable/ModelTaskExpand/Tools/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Tools = () => {
-	return <h1>Tools</h1>;
-};
-
-export default Tools;
