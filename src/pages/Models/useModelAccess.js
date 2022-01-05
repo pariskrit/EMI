c5065d208ceac80ba70f770a40f7d4726f42f101
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2022-01-05 12:31:19 +0545

    added model acces hook

--- src/pages/Models/useModelAccess.js
+++ src/pages/Models/useModelAccess.js
@@ -2,7 +2,13 @@ import { useHistory } from "react-router-dom";
 const useModelAccess = () => {
 	const history = useHistory();
 	const { position } = JSON.parse(localStorage.getItem("me"));
-	if (position === null) {
+	const access = position?.modelAccess;
+	if (
+		position !== null &&
+		(access === "F" || access === "E" || access === "R")
+	) {
+		return;
+	} else {
 		if (history.length > 1) {
 			history.goBack();
 		} else {
