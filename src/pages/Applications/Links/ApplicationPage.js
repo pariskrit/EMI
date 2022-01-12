a09b598de54d7e9451b3bb94a619dd43210d608e
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2022-01-12 11:18:20 +0545

    working on navigation issue

--- src/pages/Applications/Links/ApplicationPage.js
+++ src/pages/Applications/Links/ApplicationPage.js
@@ -2,13 +2,12 @@ import React from "react";
 import Applications from "..";
 import AccessRoute from "components/HOC/AccessRoute";
 import { routeList } from "./routeList";
-import roles from "helpers/roles";
 
 export default function ApplicationPage() {
 	return (
 		<Applications>
 			{routeList.map(({ id, ...route }) => (
-				<AccessRoute key={id} {...route} exact user={roles.superAdmin} />
+				<AccessRoute key={id} {...route} exact />
 			))}
 		</Applications>
 	);
--- src/pages/Clients/Links/ClientPage.js
+++ src/pages/Clients/Links/ClientPage.js
@@ -5,23 +5,12 @@ import ClientList from "pages/Clients/ClientList/ClientList";
 import Client from "..";
 import ClientDetails from "../ClientDetailScreen/ClientDetails";
 import AccessRoute from "components/HOC/AccessRoute";
-import roles from "helpers/roles";
 
 export default function ClientPage() {
 	return (
 		<Client>
-			<AccessRoute
-				path={clientsPath}
-				exact
-				component={ClientList}
-				user={roles.superAdmin}
-			/>
-			<AccessRoute
-				path={clientDetailPath}
-				exact
-				component={ClientDetails}
-				user={roles.superAdmin}
-			/>
+			<AccessRoute path={clientsPath} exact component={ClientList} />
+			<AccessRoute path={clientDetailPath} exact component={ClientDetails} />
 		</Client>
 	);
 }
