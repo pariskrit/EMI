07dab6a6335ce245b63ce7cbb56113dd0f0f7f6d
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2022-01-14 08:57:55 +0545

    added role route in site page

--- src/pages/Clients/Sites/Links/SitePage.js
+++ src/pages/Clients/Sites/Links/SitePage.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import roles from "helpers/roles";
 import Site from "pages/Clients/Sites";
 import { routeList } from "./routeList";
+import RoleRoute from "components/HOC/RoleRoute";
 
 export default function SitePage() {
 	return (
 		<Site>
 			{routeList.map(({ id, ...route }) => (
-				<Route key={id} {...route} exact />
+				<RoleRoute key={id} {...route} exact roles={[roles.superAdmin]} />
 			))}
 		</Site>
 	);
