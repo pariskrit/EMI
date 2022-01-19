e751c6b95b3a2f994ab9977428163e376a3f212a
ashokganika <ganika.ashok@webexpertsnepal.com>
2022-01-19 13:56:37 +0545

    sidebar selected item color adjusted according to application color

--- src/components/Layouts/NavbarWrapper/Navbar.js
+++ src/components/Layouts/NavbarWrapper/Navbar.js
@@ -28,6 +28,7 @@ import ColourConstants from "helpers/colourConstants";
 import { userProfilePath, applicationPortalPath } from "helpers/routePaths";
 import navList from "./navList";
 import "./style.scss";
+import { LightenDarkenColor } from "helpers/lightenDarkenColor";
 
 // Size constants
 const drawerWidth = 240;
@@ -86,10 +87,16 @@ function Navbar({ userLogOut, isApplicationPortal = false, isLoading }) {
 				height: 5,
 			},
 			"&::-webkit-scrollbar-track": {
-				background: "#e2b466",
+				background:
+					application === null
+						? "#e2b466"
+						: LightenDarkenColor(application?.color, -60),
 			},
 			"&::-webkit-scrollbar-thumb": {
-				background: "#734d0f45",
+				background:
+					application === null
+						? "#734d0f45"
+						: LightenDarkenColor(application?.color, 25),
 				borderRadius: 12,
 			},
 		},
@@ -171,7 +178,10 @@ function Navbar({ userLogOut, isApplicationPortal = false, isLoading }) {
 			justifyContent: "center",
 		},
 		currentItemBackground: {
-			backgroundColor: ColourConstants.navCurrentItem,
+			backgroundColor:
+				application === null
+					? ColourConstants.navCurrentItem
+					: LightenDarkenColor(application?.color, -20),
 		},
 		navIconContainer: {
 			display: "flex",
