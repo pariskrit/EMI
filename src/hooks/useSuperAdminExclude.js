473c10e8e61ff1db4b5ef7a0132f3e9025cd49cc
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2022-01-19 14:02:32 +0545

    added session storage in hook useSuperAdminExclude

--- src/hooks/useSuperAdminExclude.js
+++ src/hooks/useSuperAdminExclude.js
@@ -4,7 +4,9 @@ import { useHistory } from "react-router-dom";
 const useSuperAdminExclude = () => {
 	const history = useHistory();
 	const isLoading = useSelector((state) => state.commonData.isLoading);
-	const { position } = JSON.parse(localStorage.getItem("me"));
+	const { position } =
+		JSON.parse(sessionStorage.getItem("me")) ||
+		JSON.parse(localStorage.getItem("me"));
 	if (!isLoading) {
 		if (position === null) {
 			if (history.length > 1) {
