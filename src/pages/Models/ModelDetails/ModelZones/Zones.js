b30d528dd141ce9a10cbad74f814a11a7ea63d8c
Mukesh Dhungana <Mukesh0702>
2022-01-27 13:12:30 +0545

    fix row image design

--- src/pages/Models/ModelDetails/ModelZones/Zones.js
+++ src/pages/Models/ModelDetails/ModelZones/Zones.js
@@ -38,7 +38,20 @@ function Zones({ modelId, state, dispatch }) {
 					response[0].data.map((d) => ({
 						...d,
 						imageURL: (
-							<img src={d?.imageURL} alt="img" style={{ width: "100px" }} />
+							<div
+								className="image-wrapper"
+								style={{ width: "100px", height: "70px" }}
+							>
+								<img
+									src={d?.imageURL}
+									alt="img"
+									style={{
+										width: "100px",
+										height: "100%",
+										objectFit: "contain",
+									}}
+								/>
+							</div>
 						),
 					}))
 				);
@@ -46,7 +59,20 @@ function Zones({ modelId, state, dispatch }) {
 					response[0].data.map((d) => ({
 						...d,
 						imageURL: (
-							<img src={d?.imageURL} alt="img" style={{ width: "100px" }} />
+							<div
+								className="image-wrapper"
+								style={{ width: "100px", height: "70px" }}
+							>
+								<img
+									src={d?.imageURL}
+									alt="img"
+									style={{
+										width: "100px",
+										height: "100%",
+										objectFit: "contain",
+									}}
+								/>
+							</div>
 						),
 					}))
 				);
