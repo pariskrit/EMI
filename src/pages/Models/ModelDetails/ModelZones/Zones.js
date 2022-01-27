45c1371e8d7959bb7486c73ff0e7e131d730ee62
ashok22ganika <94095007+ashok22ganika@users.noreply.github.com>
2022-01-27 14:20:20 +0545

    Ashok (#53)
    * sidebar selected item color adjusted according to application color
    
    * nvabr scroll track color adjusted
    
    * message alerts issue and loading after edit/add fixed
    
    * drag and drop patching issue resolved

--- src/pages/Models/ModelDetails/ModelZones/Zones.js
+++ src/pages/Models/ModelDetails/ModelZones/Zones.js
@@ -70,6 +70,21 @@ function Zones({ modelId, state, dispatch }) {
 	}, []);
 
 	// handle dragging of zone
+	const setPositionForPayload = (e, listLength) => {
+		const { destination } = e;
+		if (destination.index === listLength - 1) {
+			return originalZoneList[destination.index]?.pos + 1;
+		}
+		if (destination.index === 0) {
+			return originalZoneList[destination.index]?.pos - 1;
+		}
+		return (
+			(+originalZoneList[destination.index]?.pos +
+				+originalZoneList[e.destination.index + 1]?.pos) /
+			2
+		);
+	};
+
 	const handleDragEnd = async (e) => {
 		if (!e.destination) {
 			return;
@@ -85,10 +100,7 @@ function Zones({ modelId, state, dispatch }) {
 				{
 					path: "pos",
 					op: "replace",
-					value:
-						e.destination.index > e.source.index
-							? +originalZoneList[e.destination.index]?.pos + 1
-							: +originalZoneList[e.destination.index]?.pos - 1,
+					value: setPositionForPayload(e, originalZoneList.length),
 				},
 			];
 			const response = await patchModelVersionZones(e.draggableId, payloadBody);
