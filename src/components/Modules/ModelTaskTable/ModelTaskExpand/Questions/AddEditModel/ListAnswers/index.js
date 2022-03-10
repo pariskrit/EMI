f776349d610ee73dc7ad232a953a5529d7a84dbf
sapkota.rujal <sapkota.rujal@webexpertsnepal.com>
2022-03-10 11:02:43 +0545

    Fix: Add options in model-task-question model

--- src/components/Modules/ModelTaskTable/ModelTaskExpand/Questions/AddEditModel/ListAnswers/index.js
+++ src/components/Modules/ModelTaskTable/ModelTaskExpand/Questions/AddEditModel/ListAnswers/index.js
@@ -35,6 +35,11 @@ const me =
 	JSON.parse(localStorage.getItem("me"));
 
 const useStyles = makeStyles({
+	add: {
+		marginBottom: 12,
+		display: "flex",
+		gap: 5,
+	},
 	options: { width: "100%" },
 	list: { display: "flex", flexDirection: "column" },
 	individualList: {
@@ -77,6 +82,7 @@ const useStyles = makeStyles({
 		color: "#FFFFFF",
 		fontFamily: "Roboto Condensed",
 		fontWeight: "bold",
+		height: 35,
 	},
 });
 
@@ -278,7 +284,7 @@ function ListAnswers({ type, modelVersionTaskQuestionID, getError }) {
 				/>
 				<div className={classes.list}>
 					{addNew ? (
-						<div ref={ref} style={{ marginBottom: 12 }}>
+						<div ref={ref} className={classes.add}>
 							<TextField
 								error={error.name === null ? false : true}
 								helperText={error.name === null ? null : error.name}
@@ -288,11 +294,11 @@ function ListAnswers({ type, modelVersionTaskQuestionID, getError }) {
 								onChange={(e) => setState({ name: e.target.value })}
 								fullWidth
 								onKeyDown={handleEnterPress}
-								style={{ marginBottom: 12 }}
+								style={{ marginBottom: 12, width: "94%" }}
 							/>
 							<FormGroup style={{ width: "12%" }}>
 								<FormControlLabel
-									style={{ marginLeft: 0 }}
+									style={{ marginLeft: 0, marginRight: 3 }}
 									control={
 										<EMICheckbox
 											state={input.raiseDefect}
