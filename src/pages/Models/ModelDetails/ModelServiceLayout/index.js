3e0c60c930b9539ef577ce90519fefc8144703f0
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2022-03-16 16:46:14 +0545

    fixed access issue

--- src/pages/Models/ModelDetails/ModelServiceLayout/index.js
+++ src/pages/Models/ModelDetails/ModelServiceLayout/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ServiceLayoutUI from "components/Modules/ServiceLayoutUI";
 
 function ServiceLayout({ state, dispatch, access, modelId }) {
-	return <ServiceLayoutUI modelId={modelId} />;
+	return <ServiceLayoutUI modelId={modelId} access={access} />;
 }
 
 export default ServiceLayout;
