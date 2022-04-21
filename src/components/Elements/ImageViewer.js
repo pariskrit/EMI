0e2535c8e62bed85b78513127661bc4471d8ed86
Mukesh Dhungana <Mukesh0702>
2022-04-21 11:21:27 +0545

    fix image viewer overflow issue

--- src/components/Elements/ImageViewer.js
+++ src/components/Elements/ImageViewer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Dialog, DialogContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+	dailogBox: {
+		overflowX: "hidden!important",
+	},
+});
 function ImageViewer({ open, onClose, imgSource }) {
+	const classes = useStyles();
 	return (
 		<Dialog open={open} onClose={onClose} className="image_Viewer_Dialouge">
-			<DialogContent>
+			<DialogContent className={classes.dailogBox}>
 				<img src={imgSource} alt="" />
 			</DialogContent>
 		</Dialog>
