400fac83172fb9e485982be50e17ab904eb19269
ashok22ganika <94095007+ashok22ganika@users.noreply.github.com>
2022-05-10 08:59:57 +0545

    Service details screen (#194)
    * stage tab issue fixed
    
    * route setup
    
    * details route setup
    
    * service setup
    
    * created service detail common header component
    
    * api of get change status integrated
    
    * service details fetched
    
    * service name added to header
    
    * status added in service details header
    
    * details fields added in service details
    
    * changed in model detail
    
    * created progress bar
    
    * progress bar added
    
    * added notes
    
    * notes fetched
    
    * people added
    
    * progressbar added
    
    * service info added
    
    * real value added in progressbar
    
    * details tile api setup
    
    * updating field in details
    
    * disable fields when updating
    
    * role issue fixed
    
    * icon component created
    
    * added people tile in details
    
    * read only added in details tile
    
    * route issue fixed
    
    * role api added in service-details-screen
    
    * detail tile added
    
    * icon added to service information
    
    * icon added to service information
    
    * datetime issue fixed in details
    
    * validation added for service details change status

--- src/pages/Services/ServiceDetails/Detail/changeStatusPopUp.js
+++ src/pages/Services/ServiceDetails/Detail/changeStatusPopUp.js
@@ -5,6 +5,7 @@ import {
 	DialogTitle,
 	LinearProgress,
 } from "@material-ui/core";
+import * as yup from "yup";
 import { makeStyles } from "@material-ui/core/styles";
 import AddDialogStyle from "styles/application/AddDialogStyle";
 import { useState } from "react";
@@ -17,8 +18,23 @@ import { getStatusChanges } from "services/clients/sites/siteApplications/status
 import { changeServiceStatus } from "services/services/serviceDetails/detail";
 import TextFieldContainer from "components/Elements/TextFieldContainer";
 import { changeStatusReason } from "constants/serviceDetails";
+import {
+	generateErrorState,
+	handleSort,
+	handleValidateObj,
+} from "helpers/utils";
+import ErrorInputFieldWrapper from "components/Layouts/ErrorInputFieldWrapper";
 
 const ADD = AddDialogStyle();
+
+const schema = yup.object({
+	SelectedchangeStatusReason: yup
+		.string("This field must be a string")
+		.required("This field is required"),
+	selectedServiceStatus: yup
+		.string("This field must be a string")
+		.required("This field is required"),
+});
 
 const useStyles = makeStyles({
 	dialogContent: {
@@ -36,33 +52,47 @@ function ChangeStatusPopup({ open, onClose, status }) {
 	const [selectedServiceStatus, setSelectedServiceStatus] = useState({});
 	const [SelectedchangeStatusReason, setChnageStatusReason] = useState({});
 	const [otherReason, setOtherReason] = useState(null);
+	const [errors, setErrors] = useState({
+		selectedServiceStatus: null,
+		SelectedchangeStatusReason: null,
+	});
 
 	const dispatch = useDispatch();
 	const [, serviceDispatch] = useContext(ServiceContext);
 	const { id } = useParams();
 
 	const handleCreateProcess = async () => {
-		if (
-			SelectedchangeStatusReason?.id === undefined ||
-			selectedServiceStatus?.id === undefined
-		) {
-			return;
-		}
 		setIsUpdating(true);
+		setErrors({
+			selectedServiceStatus: null,
+			SelectedchangeStatusReason: null,
+		});
 		try {
-			const response = await changeServiceStatus(id, {
-				changeStatusReasonID: SelectedchangeStatusReason?.id,
-				status: selectedServiceStatus.id,
-				otherReason: otherReason,
+			const localChecker = await handleValidateObj(schema, {
+				selectedServiceStatus: selectedServiceStatus?.id,
+				SelectedchangeStatusReason: SelectedchangeStatusReason?.id,
 			});
-			if (!response.status)
-				dispatch(showError(response.data || "Could Not Update Service Status"));
-			else {
-				serviceDispatch({
-					type: "SET_SERVICE_STATUS",
-					payload: selectedServiceStatus?.id,
+			if (!localChecker.some((el) => el.valid === false)) {
+				const response = await changeServiceStatus(id, {
+					changeStatusReasonID: SelectedchangeStatusReason?.id,
+					status: selectedServiceStatus.id,
+					otherReason: otherReason,
 				});
-				onClosePop();
+				if (!response.status)
+					dispatch(
+						showError(response.data || "Could Not Update Service Status")
+					);
+				else {
+					serviceDispatch({
+						type: "SET_SERVICE_STATUS",
+						payload: selectedServiceStatus?.id,
+					});
+					onClosePop();
+				}
+			} else {
+				const newErrors = generateErrorState(localChecker);
+				setErrors({ ...errors, ...newErrors });
+				setIsUpdating(false);
 			}
 		} catch (error) {
 			console.log(error);
@@ -75,6 +105,10 @@ function ChangeStatusPopup({ open, onClose, status }) {
 		setOtherReason(null);
 		setChnageStatusReason({});
 		setSelectedServiceStatus({});
+		setErrors({
+			selectedServiceStatus: null,
+			SelectedchangeStatusReason: null,
+		});
 		onClose();
 	};
 
@@ -144,33 +178,52 @@ function ChangeStatusPopup({ open, onClose, status }) {
 			</ADD.ActionContainer>
 
 			<DialogContent className={classes.dialogContent}>
-				<DyanamicDropdown
-					dataSource={changeStatusReason.filter((x) => {
-						if (x.showIn === status?.serviceDetail?.status) return x;
-						return false;
-					})}
-					onChange={onModelStatusChange}
-					selectedValue={selectedServiceStatus}
-					selectdValueToshow="name"
-					columns={[{ name: "name", id: 1 }]}
-					dataHeader={[{ name: "Status", id: 1 }]}
-					label="Change Status"
-					width="100%"
-					required
-				/>
+				<ErrorInputFieldWrapper
+					errorMessage={
+						errors.selectedServiceStatus === null
+							? null
+							: errors.selectedServiceStatus
+					}
+				>
+					<DyanamicDropdown
+						dataSource={changeStatusReason.filter((x) => {
+							if (x.showIn === status?.serviceDetail?.status) return x;
+							return false;
+						})}
+						onChange={onModelStatusChange}
+						selectedValue={selectedServiceStatus}
+						selectdValueToshow="name"
+						columns={[{ name: "name", id: 1 }]}
+						dataHeader={[{ name: "Status", id: 1 }]}
+						label="Change Status"
+						width="100%"
+						required
+						isError={errors.selectedServiceStatus === null ? false : true}
+					/>
+				</ErrorInputFieldWrapper>
 				<div style={{ marginBottom: 14 }}></div>
-				<DyanamicDropdown
-					dataSource={serviceStatuses}
-					onChange={(val) => setChnageStatusReason(val)}
-					selectedValue={SelectedchangeStatusReason}
-					selectdValueToshow="name"
-					columns={[{ name: "name", id: 1 }]}
-					dataHeader={[{ name: "Status Reason", id: 1 }]}
-					label="Change Status Reason"
-					showHeader
-					required
-					width="100%"
-				/>
+				<ErrorInputFieldWrapper
+					errorMessage={
+						errors.SelectedchangeStatusReason === null
+							? null
+							: errors.SelectedchangeStatusReason
+					}
+				>
+					<DyanamicDropdown
+						dataSource={serviceStatuses}
+						onChange={(val) => setChnageStatusReason(val)}
+						selectedValue={SelectedchangeStatusReason}
+						selectdValueToshow="name"
+						columns={[{ name: "name", id: 1 }]}
+						dataHeader={[{ name: "Status Reason", id: 1 }]}
+						label="Change Status Reason"
+						showHeader
+						required
+						handleSort={handleSort}
+						width="100%"
+						isError={errors.SelectedchangeStatusReason === null ? false : true}
+					/>
+				</ErrorInputFieldWrapper>
 				<div style={{ marginBottom: 14 }}></div>
 				{SelectedchangeStatusReason.name === "Other" && (
 					<TextFieldContainer
